fc2d1e719d0d279825b142d7ee958bdecf5a1307
Mirac318 <mirac318@outlook.com>
2024-02-04 04:40:37 +0800

    Simulate 100 times with noise

--- main.py
+++ main.py
@@ -9,21 +9,30 @@ Q_arr = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], dtype=np.float64
 R_arr = np.array([0.1, 0.1, 0.1], dtype=np.float64)
 kf_sub = kf.kalman_filter(initState=State, P_array=P_arr, Q_array=Q_arr, R_array=R_arr)
 
+noise = np.round(np.random.normal(0, 0.1, 100), 2)
+noise_matx = np.array(noise)
+noise = np.round(np.random.normal(0, 0.1, 100), 2)
+noise_maty = np.array(noise)
+noise = np.round(np.random.normal(0, 0.1, 100), 2)
+noise_matz = np.array(noise)
+
 # Creating figure
 fig = plt.figure(figsize=(10, 7))
 ax = plt.axes(projection="3d")
 
 Observe = numpy.matlib.mat([[0.10], [0.10], [0.10]], dtype=np.float64)
-for i in range (1, 10):
+for i in range (1, 50):
+    noiseObs = numpy.matlib.mat([[noise_matx[i]], [noise_maty[i]], [noise_matz[i]]], dtype=np.float64)
     kf_sub.Predict(dT=0.01)
-    kf_sub.Update(Obs=Observe)
+    kf_sub.Update(Obs=Observe+noiseObs)
     cur_state = kf_sub.getState()
     ax.scatter3D(cur_state[0], cur_state[1], cur_state[2], color="green")
 
 Observe = numpy.matlib.mat([[-0.10], [-0.20], [0.00]], dtype=np.float64)
-for i in range (1, 10):
+for i in range (50, 100):
+    noiseObs = numpy.matlib.mat([[noise_matx[i]], [noise_maty[i]], [noise_matz[i]]], dtype=np.float64)
     kf_sub.Predict(dT=0.01)
-    kf_sub.Update(Obs=Observe)
+    kf_sub.Update(Obs=Observe+noiseObs)
     cur_state = kf_sub.getState()
     ax.scatter3D(cur_state[0], cur_state[1], cur_state[2], color="green")
 
