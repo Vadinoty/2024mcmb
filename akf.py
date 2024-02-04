40982202ab6f8728a15d170678641b9ecb1a0ed9
Mirac318 <mirac318@outlook.com>
2024-02-04 20:06:47 +0800

    added akf

---
+++ akf.py
@@ -0,0 +1,64 @@
+import numpy.matlib
+import numpy as np
+
+
+# State Definition: 9 dims, positionXYZ, VelocityXYZ, AccXYZ
+# x, y, z, vx, vy, vz, ax, ay, az, 
+
+class kalman_filter:
+    Xe = numpy.matlib.zeros(shape=(9, 1), dtype=np.float64) # Estimate
+    Xp = numpy.matlib.zeros(shape=(9, 1), dtype=np.float64) # Predict
+    F = numpy.matlib.zeros(shape=(9, 9), dtype=np.float64)
+    H = numpy.matlib.zeros(shape=(3, 9), dtype=np.float64)
+    Pe = numpy.matlib.zeros(shape=(9, 9), dtype=np.float64)
+    Pp = numpy.matlib.zeros(shape=(9, 9), dtype=np.float64)
+    Q = numpy.matlib.zeros(shape=(9, 9), dtype=np.float64)
+    R = numpy.matlib.zeros(shape=(3, 3), dtype=np.float64)
+    K = numpy.matlib.zeros(shape=(9, 3), dtype=np.float64)
+    d = 1 # forget
+    t = 1
+
+    def __init__(self, initState, P_array, Q_array, R_array):
+        self.Xe = initState
+        self.Pe = np.diag(P_array)
+        self.Q = np.diag(Q_array)
+        self.R = np.diag(R_array)
+        self.F = numpy.matlib.eye(n=9, M=9)
+        self.H = numpy.matlib.mat([[0, 0, 0, 0, 0, 0, 1, 0, 0],
+                              [0, 0, 0, 0, 0, 0, 0, 1, 0],
+                              [0, 0, 0, 0, 0, 0, 0, 0, 1]], dtype=np.float64)
+        
+    def Predict(self, dT):
+        self.F = numpy.matlib.mat([[1, 0, 0, dT, 0, 0, 0, 0, 0],
+                              [0, 1, 0, 0, dT, 0, 0, 0, 0],
+                              [0, 0, 1, 0, 0, dT, 0, 0, 0],
+                              [0, 0, 0, 1, 0, 0, dT, 0, 0],
+                              [0, 0, 0, 0, 1, 0, 0, dT, 0],
+                              [0, 0, 0, 0, 0, 1, 0, 0, dT],
+                              [0, 0, 0, 0, 0, 0, 1, 0, 0],
+                              [0, 0, 0, 0, 0, 0, 0, 1, 0],
+                              [0, 0, 0, 0, 0, 0, 0, 0, 1]])
+        self.Xp = self.F * self.Xe # predict
+        self.Pp = self.F * self.Pe * self.F.T + self.Q
+
+    def Update(self, Obs):
+        self.K = self.Pp * self.H.T * np.linalg.inv(self.H * self.Pp * self.H.T + self.R.T)
+        self.Xe = self.Xp + self.K * (Obs - self.H * self.Xp)
+        self.Pe = (numpy.matlib.eye(n=9, M=9) - self.K * self.H) * self.Pe
+    
+    def AdaptiveUpdate(self, Obs):
+        self.t = self.t + 1
+        self.d = 1 / self.t
+        epsilon = Obs - self.H * self.Xp
+        self.R = (1 - self.d) * self.R + self.d * (epsilon * epsilon.T - self.H * self.Pp * self.H.T ) # sage Husa update
+        print(self.R)
+        self.K = self.Pp * self.H.T * np.linalg.inv(self.H * self.Pp * self.H.T + self.R.T)
+        self.Xe = self.Xp + self.K * epsilon
+        self.Pe = (numpy.matlib.eye(n=9, M=9) - self.K * self.H) * self.Pe
+
+    def getState(self):
+        return self.Xe
+    
+    def printRQ(self):
+        print(self.R)
+        print(self.Q)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import kf
+import akf
 import numpy as np
 import numpy.matlib
 import matplotlib.pyplot as plt
@@ -7,35 +7,47 @@ State = numpy.matlib.mat([0, 0, 0, 0, 0, 0, 0, 0, 0], dtype=np.float64).reshape(
 P_arr = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0], dtype=np.float64)
 Q_arr = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], dtype=np.float64)
 R_arr = np.array([0.1, 0.1, 0.1], dtype=np.float64)
-kf_sub = kf.kalman_filter(initState=State, P_array=P_arr, Q_array=Q_arr, R_array=R_arr)
+kf_sub = akf.kalman_filter(initState=State, P_array=P_arr, Q_array=Q_arr, R_array=R_arr)
 
-noise = np.round(np.random.normal(0, 0.1, 100), 2)
+noise = np.round(np.random.normal(0, 0.1, 150), 2)
 noise_matx = np.array(noise)
-noise = np.round(np.random.normal(0, 0.1, 100), 2)
+noise = np.round(np.random.normal(0, 0.1, 150), 2)
 noise_maty = np.array(noise)
-noise = np.round(np.random.normal(0, 0.1, 100), 2)
+noise = np.round(np.random.normal(0, 0.1, 150), 2)
 noise_matz = np.array(noise)
 
 # Creating figure
 fig = plt.figure(figsize=(10, 7))
 ax = plt.axes(projection="3d")
 
-Observe = numpy.matlib.mat([[0.10], [0.10], [0.10]], dtype=np.float64)
+Observe = numpy.matlib.mat([[1.00], [0.00], [0.00]], dtype=np.float64)
 for i in range (1, 50):
+    print(i)
     noiseObs = numpy.matlib.mat([[noise_matx[i]], [noise_maty[i]], [noise_matz[i]]], dtype=np.float64)
     kf_sub.Predict(dT=0.01)
-    kf_sub.Update(Obs=Observe+noiseObs)
+    kf_sub.AdaptiveUpdate(Obs=Observe+noiseObs)
     cur_state = kf_sub.getState()
     ax.scatter3D(cur_state[0], cur_state[1], cur_state[2], color="green")
 
-Observe = numpy.matlib.mat([[-0.10], [-0.20], [0.00]], dtype=np.float64)
+Observe = numpy.matlib.mat([[0.00], [1.00], [0.00]], dtype=np.float64)
 for i in range (50, 100):
+    print(i)
     noiseObs = numpy.matlib.mat([[noise_matx[i]], [noise_maty[i]], [noise_matz[i]]], dtype=np.float64)
     kf_sub.Predict(dT=0.01)
-    kf_sub.Update(Obs=Observe+noiseObs)
+    kf_sub.AdaptiveUpdate(Obs=Observe+noiseObs)
     cur_state = kf_sub.getState()
     ax.scatter3D(cur_state[0], cur_state[1], cur_state[2], color="green")
+
+Observe = numpy.matlib.mat([[0.00], [0.00], [1.00]], dtype=np.float64)
+for i in range (100, 150):
+    print(i)
+    noiseObs = numpy.matlib.mat([[noise_matx[i]], [noise_maty[i]], [noise_matz[i]]], dtype=np.float64)
+    kf_sub.Predict(dT=0.01)
+    kf_sub.AdaptiveUpdate(Obs=Observe+noiseObs)
+    cur_state = kf_sub.getState()
+    ax.scatter3D(cur_state[0], cur_state[1], cur_state[2], color="green")
+
+kf_sub.printRQ()
 
 plt.title("XYZ")
-# show plot
-plt.show()
+plt.show() # show plot
